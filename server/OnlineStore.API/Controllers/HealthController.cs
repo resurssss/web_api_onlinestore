b9b1042510233bde5f8e255227dcfc6ad4a0f95d
lyuba <lyubaerofeewaa@yandex.ru>
2026-04-23 23:17:57 +0300

    Did mistake

--- server/OnlineStore.API/Controllers/HealthController.cs
+++ server/OnlineStore.API/Controllers/HealthController.cs
@@ -22,7 +22,8 @@ namespace OnlineStore.API.Controllers
         {
             Response.Headers.Append("X-Instance-Id", _instanceId);
             HealthCheckCounter.Inc();
-            return Ok(new { status = "healthy", instance = _instanceId });
+            //return Ok(new { status = "healthy", instance = _instanceId });
+            return StatusCode(500, new { status = "unhealthy", instance = _instanceId });
         }
     }
 }
